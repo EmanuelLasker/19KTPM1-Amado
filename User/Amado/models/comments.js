4945ac8c59438907d1e13746f940ecf27de27ef5
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2021-12-24 14:34:04 +0700

    Store comment in DB

--- User/Amado/models/comments.js
+++ User/Amado/models/comments.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const comment = new mongoose.Schema({
-    customer: String,
     customerName: String,
     productName: String,
     comment: String,
-    order: String,
     date: String
 },{ versionKey: null });
 
