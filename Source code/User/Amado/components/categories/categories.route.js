2c3f3c1769fc41bf2c7729caec3b280bce336ba1
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2022-05-13 20:56:40 +0700

    Fix bugs in categories

--- Source code/User/Amado/components/categories/categories.route.js
+++ Source code/User/Amado/components/categories/categories.route.js
@@ -2,14 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 const categoriesController = require('../categories/CategoriesController');
-// const ExpressRedisCache = require('express-redis-cache');
-// const cache1 = ExpressRedisCache({
-//     expire: 60, // optional: expire every 10 seconds
-// })
-// const cache2 = ExpressRedisCache({
-//     expire: 5, // optional: expire every 10 seconds
-// })
-
 router.get('/:id',categoriesController.getList);
 router.get('/:id/page/:page', categoriesController.getListAtPage);
 
