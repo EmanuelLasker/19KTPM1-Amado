f097b2eba0621e742f22c854d184564f767b22fc
Lê Quang Tấn Long <63658369+lequangtanlong@users.noreply.github.com>
2022-05-13 20:06:38 +0700

    Remove backup folder

--- Backup/User/Amado/components/categories/categories.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const categoriesController = require('../categories/CategoriesController');
-
-router.get('/:id', categoriesController.getList);
-router.get('/:id/page/:page', categoriesController.getListAtPage);
-
-module.exports = router;
